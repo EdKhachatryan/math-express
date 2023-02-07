7b6792c196ea562b980b543941a300654840a6a8
EdKhachatryan <eduard.khachatrian@gmail.com>
2023-02-07 17:09:11 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'math-express-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'math-express';
+}
